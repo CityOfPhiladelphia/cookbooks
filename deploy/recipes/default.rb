51eec64a54f0bcfbe9d49b81ced1fbec008d3cb5
Gabriel Farrell <g@gsf.io>
2015-01-16 16:19:19 -0500

    Add deploy::default override

---
+++ deploy/recipes/default.rb
@@ -0,0 +1,20 @@
+include_recipe 'deploy'
+include_recipe "nginx::service"
+
+node[:deploy].each do |application, deploy|
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    app application
+    deploy_data deploy
+  end
+
+  nginx_web_app application do
+    application deploy
+    cookbook "nginx"
+  end
+end
