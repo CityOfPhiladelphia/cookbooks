5c0cf1baaad50e3add644f13fd066bbb431ece4f
Gabriel Farrell <g@gsf.io>
2015-02-19 18:18:39 +0000

    Forgot template pointer in nginx recipe

--- wordpress/recipes/nginx.rb
+++ wordpress/recipes/nginx.rb
@@ -6,3 +6,11 @@ template "/etc/nginx/sites-available/default" do
   group "root"
   mode 0644
 end
+
+# Set variables in WP environment
+template "/etc/nginx/wordpress_fastcgi_params" do
+  source "wordpress_fastcgi_params.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
