2467aa2575ba59d7e8f027ae80c5fd708cb575d0
Gabriel Farrell <g@gsf.io>
2015-05-05 10:33:03 -0400

    Clean up config

--- deploy/recipes/wordpress.rb
+++ deploy/recipes/wordpress.rb
@@ -1,4 +1,5 @@
 node[:deploy].each do |application, deploy|
+
   opsworks_deploy_dir do
     user deploy[:user]
     group deploy[:group]
@@ -27,9 +28,9 @@ node[:deploy].each do |application, deploy|
     action :restart
   end
 
-  # Set deploy variables in WP environment
-  template "/etc/nginx/wordpress_deploy_fastcgi_params" do
-    source "wordpress_deploy_fastcgi_params.erb"
+  # Set variables in WP environment
+  template "/etc/nginx/wordpress_fastcgi_params" do
+    source "wordpress_fastcgi_params.erb"
     owner "root"
     group "root"
     mode 0644
