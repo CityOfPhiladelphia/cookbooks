4eac332287c9e7e73365173b053abc2b7694feaf
Gabriel Farrell <g@gsf.io>
2015-06-16 12:20:30 -0400

    Update composer

--- wordpress/recipes/php.rb
+++ wordpress/recipes/php.rb
@@ -5,7 +5,7 @@ package "php5-curl"
 
 bash "install composer" do
   code <<-EOH
-  curl -sS https://getcomposer.org/download/1.0.0-alpha9/composer.phar > /usr/local/bin/composer
+  curl -sS https://getcomposer.org/download/1.0.0-alpha10/composer.phar > /usr/local/bin/composer
   chmod 755 /usr/local/bin/composer
   EOH
 end
