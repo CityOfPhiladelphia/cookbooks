6bbd172db0f386c0c5ea9b21a6abb6156a489c45
Gabriel Farrell <g@gsf.io>
2015-06-17 16:44:00 -0400

    Management has its own cookbook now

--- deploy/recipes/management.rb
+++
@@ -1,8 +0,0 @@
-db = node['management']['db']
-
-bash "sync staging db with production" do
-  code <<-EOH
-    mysqldump --single-transaction -C -h#{db['prod']['host']} -uwp -p#{db['prod']['pass']} wp | \
-    mysql -h#{db['stag']['host']} -uwp -p#{db['stag']['pass']} wp
-  EOH
-end
